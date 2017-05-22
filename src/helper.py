21c1327cbe82e9656741c275d1f959a558d2dcfe
JHWu92 <jhwu92@gmail.com>
2017-05-22 17:43:13 -0400

    ana for q1 and q2

--- src/helper.py
+++ src/helper.py
@@ -15,3 +15,10 @@ def get_int(x):
     # if can't, use the package locale to parse it
     except:
         return locale.atoi(x)
+        
+        
+def save_dict_of_list(data, fn):
+    with open(fn, 'wb') as f:
+        for k, l in data.items():
+            line = '{}\t{}\n'.format(k, ' '.join([str(x) for x in l]))
+            f.write(line)
